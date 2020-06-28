26cad5465f68feb2db2ddbc6efc82e34fc00fed9
Gabriel De Moura <gabrielmoura.music@gmail.com>
2020-06-28 19:10:33 -0300

    Database sets

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gobarberDois',
+  host: 'ec2-52-72-221-20.compute-1.amazonaws.com',
+  username: 'hclfkwbfveursh',
+  password: '10ee2ad0532a6967d5ced4e2b7dfd705738c2ae0ebd0519cbe4d9c030af5cede',
+  database: 'dasl0nh55vgdth',
   define: {
     timestamps: true,
     underscored: true,
